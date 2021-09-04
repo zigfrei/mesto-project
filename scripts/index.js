38926726fba98191132e014f36e74ad9fb42033b
zigfrei <zigfrei.zf@gmail.com>
2021-09-04 19:45:55 +0300

    Добавил функциональность профилю

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+//первое условие
+const editButton = document.querySelector('.profile__edit-button');
+const popupMain = document.querySelector('.popup');
+
+function openPopup(popup){
+  popup.classList.add('popup_opened');
+}
+
+function closePopup(popup){
+  popup.classList.remove('popup_opened');
+}
+
+
+editButton.addEventListener('click', () =>{
+openPopup(popupMain);
+});
+
+popupMain.addEventListener('click', (event) =>{
+if(event.target.classList.contains('popup__close-button') || event.target.classList.contains('popup')){
+  closePopup(popupMain);
+}
+});
+
+//Второе условие
+
